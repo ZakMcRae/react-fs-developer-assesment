d92364d6faf5ee37e39a61484a2097654848554c
Zak McRae <zakmcrae@gmail.com>
2022-05-19 15:00:17 -0400

    changed repo data store and display order
    sort the data into reverse chronological order by creation date
    before storing in state
    
    renamed fetchData to fetchAndSortData for clarity

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,9 +6,9 @@ export function App() {
   const [isLoading, setIsLoading] = React.useState(true);
   const [repoData, setRepoData] = React.useState(null);
 
-  // Fetch the data from the backend api
+  // Fetch and sort the data from the backend api
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchAndSortData = async () => {
       // handle the data if response is ok
       try {
         const response = await fetch('http://localhost:4000/repos', {});
@@ -16,21 +16,23 @@ export function App() {
         if (response.ok) {
           const data = await response.json();
           setIsLoading(false);
+
+          // sort the data into reverse chronological order by creation date and store in state
+          data.sort((a: any, b: any) => (a.created_at < b.created_at ? 1 : -1));
           setRepoData(data);
         }
         // handle the error if response is not ok - waits 1s and retries the fetch
       } catch (error) {
-        setTimeout(fetchData, 1000);
+        setTimeout(fetchAndSortData, 1000);
       }
     };
 
     // set small delay so loading screen doesn't show for a fraction of a second and dispapear
-    setTimeout(fetchData, 1000);
+    setTimeout(fetchAndSortData, 1000);
   }, []);
 
   return (
     <div className="App">
-      {/* temporary component to display loading state and repo data */}
       {isLoading && <div className="loading-screen">Fetching Data...</div>}
       {repoData && <ReposList repos={repoData} />}
     </div>
