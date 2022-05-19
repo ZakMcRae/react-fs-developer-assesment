f7c80bc4480ff90a833abda292edeab130acb47e
Zak McRae <zakmcrae@gmail.com>
2022-05-19 10:27:00 -0400

    get and aggregate repo data
    read local data from file
    fetch api data using axios

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,28 @@
+import axios from 'axios';
 import { Router, Request, Response } from 'express';
+import fs from 'fs/promises';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
+  // read repos json file from /data - file can change while app running
+  const localRepoData = JSON.parse(
+    await fs.readFile('./data/repos.json', 'utf8')
+  );
+
+  // get repo data from github api
+  const response = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
+  const githubRepoData = response.data;
+
+  // combine local and github data
+  const combinedRepoData = [...localRepoData, ...githubRepoData];
+
   res.status(200);
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  // currently just returning all data combined
+  res.json(combinedRepoData);
 });
