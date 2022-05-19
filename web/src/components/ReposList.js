55a70a687040454fa89c070da192567105f34b52
Zak McRae <zakmcrae@gmail.com>
2022-05-19 15:06:57 -0400

    added headerRow for repo display list
    added a header row with column labels to repo list

--- web/src/components/ReposList.js
+++ web/src/components/ReposList.js
@@ -5,6 +5,7 @@ const RepoList = ({ repos }) => {
     <div className="repos-list-container">
       <h1 className="repos-list-title">Repos List</h1>
       <ul className="repos-list">
+        <RepoHeaderRow />
         {repos.map((repo) => (
           <RepoRow repo={repo} key={repo.id} />
         ))}
@@ -20,6 +21,17 @@ const RepoRow = ({ repo }) => {
       <p>{repo.description}</p>
       <p>{repo.language}</p>
       <p>{repo.forks_count}</p>
+    </li>
+  );
+};
+
+const RepoHeaderRow = () => {
+  return (
+    <li className="repo-row">
+      <p>Repo Name</p>
+      <p>Description</p>
+      <p>Language</p>
+      <p>Forks Count</p>
     </li>
   );
 };
