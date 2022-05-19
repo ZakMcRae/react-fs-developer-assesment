92ccab08b257cf2ec4f251f2e93f77bd7d48a961
Zak McRae <zakmcrae@gmail.com>
2022-05-19 16:33:55 -0400

    added commit details to RepoDetails pop up

--- web/src/components/RepoDetails.js
+++ web/src/components/RepoDetails.js
@@ -1,13 +1,44 @@
+import { useEffect, useState } from 'react';
+
 const RepoDetails = ({ repos, repoToDetail, setRepoToDetail }) => {
   const repo = repos.filter((item) => item.full_name === repoToDetail)[0];
 
-  // todo - work in progress
-  // add additional repo detail
+  const [commitDetails, setCommitDetails] = useState(null);
+
+  // fetch commit details for repo
+  useEffect(() => {
+    const fetchCommitDetails = async () => {
+      const response = await fetch(
+        `https://api.github.com/repos/${repo.full_name}/commits`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const commitInfo = {
+          date: data[0].commit.author.date,
+          author: data[0].commit.author.name,
+          message: data[0].commit.message,
+        };
+        setCommitDetails(commitInfo);
+        // set details to N/A if no commit info
+      } else {
+        const commitInfo = {
+          date: 'N/A',
+          author: 'N/A',
+          message: 'N/A',
+        };
+        setCommitDetails(commitInfo);
+      }
+    };
+
+    fetchCommitDetails();
+  }, [repo.full_name]);
 
   return (
     <div className="repo-detail-container">
-      <h2>Repo Details</h2>
-      <p>{repo.name}</p>
+      <h2>{repo.name}</h2>
+      <p>Last Commit</p>
+      {commitDetails && <CommitDisplay commitDetails={commitDetails} />}
       <button
         onClick={() => {
           setRepoToDetail(null);
@@ -16,6 +47,16 @@ const RepoDetails = ({ repos, repoToDetail, setRepoToDetail }) => {
         Close Detail View
       </button>
     </div>
+  );
+};
+
+const CommitDisplay = ({ commitDetails }) => {
+  return (
+    <>
+      <p>Author: {commitDetails.author}</p>
+      {commitDetails.date && <p>Date: {commitDetails.date}</p>}
+      <p>Message:{commitDetails.message}</p>
+    </>
   );
 };
 
